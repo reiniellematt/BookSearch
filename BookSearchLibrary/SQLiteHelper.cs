aac177311b165bfcfbd9049cff73eb218fcd0098
reiniellematt <erecereinielle722@gmail.com>
2018-07-20 17:21:55 +0800

    Documentation on the different methods added and SaveNewBook method added

--- BookSearchLibrary/SQLiteHelper.cs
+++ BookSearchLibrary/SQLiteHelper.cs
@@ -16,6 +16,10 @@ namespace BookSearchLibrary
     {
         private readonly string _cnnString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
 
+        /// <summary>
+        /// Gets all books from the database.
+        /// </summary>
+        /// <returns>An observable collection of books.</returns>
         public async Task<ObservableCollection<Book>> GetAllBooksAsync()
         {
             using (IDbConnection cnn = new SQLiteConnection(_cnnString))
@@ -26,6 +30,12 @@ namespace BookSearchLibrary
             }
         }
 
+        /// <summary>
+        /// Gets a certain book or books by doing a search.
+        /// </summary>
+        /// <param name="searchQuery">The keywords to search.</param>
+        /// <param name="searchOption">Wether to match it by the title or author.</param>
+        /// <returns>An observable collection of books.</returns>
         public async Task<ObservableCollection<Book>> GetBookAsync(string searchQuery, string searchOption)
         {
             using (IDbConnection cnn = new SQLiteConnection(_cnnString))
@@ -33,6 +43,18 @@ namespace BookSearchLibrary
                 var output = await cnn.QueryAsync<Book>($"SELECT * FROM Books WHERE {searchOption} LIKE '%{searchQuery}%'");
 
                 return new ObservableCollection<Book>(output);
+            }
+        }
+
+        /// <summary>
+        /// Saves a new book to the database.
+        /// </summary>
+        /// <param name="book">The book to save in the database.</param>
+        public async Task SaveNewBook(Book book)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(_cnnString))
+            {
+                await cnn.ExecuteAsync("INSERT INTO Books(Title, Author) VALUES (@Title, @Author);", book);
             }
         }
     }
